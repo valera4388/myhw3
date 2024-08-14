443067826a88162bff678b28648bdedcb8a9a8c0
mikhalev valera <mihalev.valera@mail.ru>
2024-08-14 23:50:35 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,14 @@
+public class BonusMilesService {
+
+    public int calculate (int price) {
+        int result;
+        if (price > 20) {
+            result = price / 20;
+        }
+        else {
+            result = 0;
+        }
+
+        return result;
+    }
+}
